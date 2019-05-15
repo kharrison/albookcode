d8136de32895eb8c9aef0ed1d43c76f34f75a1bf
Keith Harrison <keith@useyourloaf.com>
2019-05-15 12:03:01 +0100

    Add missing app delegate code to load view controller (Fix #1)

--- sample-code/debugging/Conflict-v2/Conflict-v2/AppDelegate.swift
+++ sample-code/debugging/Conflict-v2/Conflict-v2/AppDelegate.swift
@@ -31,4 +31,12 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
 }
