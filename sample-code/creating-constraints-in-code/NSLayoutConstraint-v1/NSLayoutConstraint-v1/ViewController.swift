c7c86f0eb571ac257ee2e37eb63d23817dc165d8
Keith Harrison <keith@useyourloaf.com>
2019-10-29 11:29:03 +0000

    Force unwrap IUO to silence Xcode 11/Swift 5 warning
    the old `NSLayoutConstraint` API uses `Any` for the item
    which generates a warning with the `view` of a VC which
    is an iUO (`UIView!`)
    
    Force unwrap to silence the warning - but this is
    another reason to avoid this outdated way of creating
    constraints. Use layout anchors instead.

--- sample-code/creating-constraints-in-code/NSLayoutConstraint-v1/NSLayoutConstraint-v1/ViewController.swift
+++ sample-code/creating-constraints-in-code/NSLayoutConstraint-v1/NSLayoutConstraint-v1/ViewController.swift
@@ -29,7 +29,6 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
     private let padding: CGFloat = 50.0
 
     private let redView: UIView = {
@@ -53,13 +52,13 @@ final class ViewController: UIViewController {
             NSLayoutConstraint(item: redView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1.0, constant: padding),
 
             // view.trailing == redView.trailing + padding
-            NSLayoutConstraint(item: view, attribute: .trailing, relatedBy: .equal, toItem: redView, attribute: .trailing, multiplier: 1.0, constant: padding),
+            NSLayoutConstraint(item: view!, attribute: .trailing, relatedBy: .equal, toItem: redView, attribute: .trailing, multiplier: 1.0, constant: padding),
 
             // redView.top == view.top + padding
             NSLayoutConstraint(item: redView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: padding),
 
             // view.bottom = redView.bottom + padding
-            NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: redView, attribute: .bottom, multiplier: 1.0, constant: padding)
-            ])
+            NSLayoutConstraint(item: view!, attribute: .bottom, relatedBy: .equal, toItem: redView, attribute: .bottom, multiplier: 1.0, constant: padding)
+        ])
     }
 }
