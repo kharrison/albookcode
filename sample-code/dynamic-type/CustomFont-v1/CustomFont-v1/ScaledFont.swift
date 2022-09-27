face3e992a62bad1a8722fcd84421aa6b85a7d97
Keith Harrison <keith@useyourloaf.com>
2022-09-27 14:33:10 +0100

    Update to new dictionary format

--- sample-code/dynamic-type/CustomFont-v1/CustomFont-v1/ScaledFont.swift
+++ sample-code/dynamic-type/CustomFont-v1/CustomFont-v1/ScaledFont.swift
@@ -1,5 +1,4 @@
-//  Created by Keith Harrison https://useyourloaf.com
-//  Copyright (c) 2018 Keith Harrison. All rights reserved.
+//  Copyright (c) 2017-2021 Keith Harrison. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
@@ -32,29 +31,29 @@ import UIKit
 /// A utility type to help you use custom fonts with
 /// dynamic type.
 ///
-/// To use supply the name of a style dictionary
-/// when creating the instance. The style
-/// dictionary should be stored as a property
-/// list file (by default in the main bundle).
-///
-/// If the plist file is invalid or missing the default
-/// system font is used.
+/// To use this type you must supply the name of a style
+/// dictionary for the font when creating the `ScaledFont`.
+/// The style dictionary should be stored as a property list
+/// file in the main bundle.
 ///
 /// The style dictionary contains an entry for each text
-/// style that uses the raw string value for each
-/// `UIFontTextStyle` as the key.
+/// style. The available text styles are:
+///
+/// - `largeTitle`, `title`, `title2`, `title3`
+/// -  `headline`, `subheadline`, `body`, `callout`
+/// -  `footnote`, `caption`, `caption2`
 ///
 /// The value of each entry is a dictionary with two keys:
 ///
-/// + fontName: A String which is the font name.
-/// + fontSize: A number which is the point size to use
-///             at the `.large` content size.
+/// + `fontName`: A `String` which is the font name.
+/// + `fontSize`: A number which is the point size to use
+///             at the `.large` (base) content size.
 ///
 /// For example to use a 17 pt Noteworthy-Bold font
 /// for the `.headline` style at the `.large` content size:
 ///
 ///     <dict>
-///         <key>UICTFontTextStyleHeadline</key>
+///         <key>headline</key>
 ///         <dict>
 ///             <key>fontName</key>
 ///             <string>Noteworthy-Bold</string>
@@ -67,27 +66,50 @@ import UIKit
 /// but if you try to use a text style that is not included
 /// in the dictionary it will fallback to the system preferred
 /// font.
+///
+/// ## Using With UIKit
+///
+/// For `UIKit`, apply the scaled font to text labels, text fields or text
+/// views:
+///
+/// ```swift
+/// let scaledFont = ScaledFont(fontName: "Noteworthy")
+/// label.font = scaledFont.font(forTextStyle: .headline)
+/// label.adjustsFontForContentSizeCategory = true
+/// ```
+///
+/// Remember to set the `adjustsFontFotContentSizeCategory` property
+/// to have the font size adjust automatically when the user changes
+/// their preferred content size.
+///
 
+@available(iOS 11.0, tvOS 11.0, watchOS 4.0, *)
 public struct ScaledFont {
-    private struct FontDescription: Decodable {
+    internal enum StyleKey: String, Decodable {
+        case largeTitle, title, title2, title3
+        case headline, subheadline, body, callout
+        case footnote, caption, caption2
+    }
+
+    internal struct FontDescription: Decodable {
         let fontSize: CGFloat
         let fontName: String
     }
 
-    private typealias StyleDictionary = [UIFont.TextStyle.RawValue: FontDescription]
-    private var styleDictionary: StyleDictionary?
+    internal typealias StyleDictionary = [StyleKey.RawValue: FontDescription]
+    internal var styleDictionary: StyleDictionary?
 
     /// Create a `ScaledFont`
     ///
     /// - Parameter fontName: Name of a plist file (without the extension)
-    ///   that contains the style dictionary used to
-    ///   scale fonts for each text style.
-    /// - Parameter bundle: Bundle containing the plist file.
-    ///   Defaults to the main bundle.
+    ///   that contains the style dictionary used to scale fonts for each
+    ///   text style.
+    /// - Parameter bundle: The `Bundle` that contains the style dictionary.
+    ///   Default is the main bundle.
 
-    public init(fontName: String, in bundle: Bundle = Bundle.main) {
+    public init(fontName: String, bundle: Bundle = .main) {
         if let url = bundle.url(forResource: fontName, withExtension: "plist"),
-            let data = try? Data(contentsOf: url)
+           let data = try? Data(contentsOf: url)
         {
             let decoder = PropertyListDecoder()
             styleDictionary = try? decoder.decode(StyleDictionary.self, from: data)
@@ -97,7 +119,7 @@ public struct ScaledFont {
     /// Get the scaled font for the given text style using the
     /// style dictionary supplied at initialization.
     ///
-    /// - Parameter textStyle: The `UIFontTextStyle` for the
+    /// - Parameter textStyle: The `UIFont.TextStyle` for the
     ///   font.
     /// - Returns: A `UIFont` of the custom font that has been
     ///   scaled for the users currently selected preferred
@@ -108,13 +130,41 @@ public struct ScaledFont {
     ///   font is returned.
 
     public func font(forTextStyle textStyle: UIFont.TextStyle) -> UIFont {
-        guard let fontDescription = styleDictionary?[textStyle.rawValue],
-            let font = UIFont(name: fontDescription.fontName, size: fontDescription.fontSize)
+        guard let styleKey = StyleKey(textStyle),
+              let fontDescription = styleDictionary?[styleKey.rawValue],
+              let font = UIFont(name: fontDescription.fontName, size: fontDescription.fontSize)
         else {
             return UIFont.preferredFont(forTextStyle: textStyle)
         }
 
         let fontMetrics = UIFontMetrics(forTextStyle: textStyle)
         return fontMetrics.scaledFont(for: font)
+    }
+}
+
+@available(iOS 11.0, tvOS 11.0, watchOS 4.0, *)
+extension ScaledFont.StyleKey {
+    init?(_ textStyle: UIFont.TextStyle) {
+        #if !os(tvOS)
+        if #available(watchOS 5.0, *) {
+            if textStyle == .largeTitle {
+                self = .largeTitle
+                return
+            }
+        }
+        #endif
+        switch textStyle {
+            case .title1: self = .title
+            case .title2: self = .title2
+            case .title3: self = .title3
+            case .headline: self = .headline
+            case .subheadline: self = .subheadline
+            case .body: self = .body
+            case .callout: self = .callout
+            case .footnote: self = .footnote
+            case .caption1: self = .caption
+            case .caption2: self = .caption2
+            default: return nil
+        }
     }
 }
