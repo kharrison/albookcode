12805f53c6eaefddbdc9227616ded7187ea76efb
Keith Harrison <keith@useyourloaf.com>
2026-01-07 15:36:43 +0000

    Update for Xcode 26. Add a scene delegate where missing

--- solutions/layout-before-auto-layout/Challenge2-2/Challenge2-2/AppDelegate.swift
+++ solutions/layout-before-auto-layout/Challenge2-2/Challenge2-2/AppDelegate.swift
@@ -30,13 +30,4 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .white
-        window?.rootViewController = RootViewController()
-        window?.makeKeyAndVisible()
-        return true
-    }
 }
--- solutions/layout-before-auto-layout/Challenge2-4/Challenge2-4/AppDelegate.swift
+++ solutions/layout-before-auto-layout/Challenge2-4/Challenge2-4/AppDelegate.swift
@@ -28,7 +28,6 @@
 
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
 }
