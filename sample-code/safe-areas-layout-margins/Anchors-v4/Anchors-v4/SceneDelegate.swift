97ab837a2cbe902416f35be4fbf9093c8c32990a
Keith Harrison <keith@useyourloaf.com>
2026-02-03 14:27:03 +0000

    Update for Xcode 26, minimal target is iOS 15 Add scene delegate Enable approachable concurrency

--- sample-code/safe-areas-layout-margins/Anchors-v4/Anchors-v4/SceneDelegate.swift
+++ sample-code/safe-areas-layout-margins/Anchors-v4/Anchors-v4/SceneDelegate.swift
@@ -1,4 +1,4 @@
-//  Copyright © 2018-2026 Keith Harrison. All rights reserved.
+//  Copyright © 2026 Keith Harrison. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
@@ -28,6 +28,23 @@
 
 import UIKit
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene, willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else {
+            return
+        }
+
+        window = UIWindow(windowScene: scene)
+        window?.backgroundColor = .white
+        let rootViewController = ViewController()
+        rootViewController.title = NSLocalizedString("Title", comment: "Title")
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
 }
