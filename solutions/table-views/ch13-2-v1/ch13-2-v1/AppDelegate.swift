94ee7771a3d53cac1ce0596b3f1dcded36c31172
Keith Harrison <keith@useyourloaf.com>
2026-03-24 11:15:50 +0000

    Update for Xcode 26, minimal target is iOS 15 Add scene delegate Enable approachable concurrency

--- solutions/table-views/ch13-2-v1/ch13-2-v1/AppDelegate.swift
+++ solutions/table-views/ch13-2-v1/ch13-2-v1/AppDelegate.swift
@@ -1,4 +1,4 @@
-//  Copyright © 2018 Keith Harrison. All rights reserved.
+//  Copyright © 2018-2026 Keith Harrison. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
@@ -30,45 +30,4 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
-
-    private let book = Book(title: "Great Expectations",
-                            author: "Charles Dickens",
-                            text: [
-                                "Chapter I",
-                                """
-        My father's family name being Pirrip, and my \
-        Christian name Philip, my infant tongue could \
-        make of both names nothing longer or more \
-        explicit than Pip.
-        """,
-                                """
-        So, I called myself Pip, and came to be called Pip.
-        """,
-                                """
-        I give Pirrip as my father's family name, on the \
-        authority of his tombstone and my sister,--Mrs. \
-        Joe Gargery, who married the blacksmith.
-        """,
-                                """
-        As I never saw my father or my mother, and never \
-        saw any likeness of either of them (for their \
-        days were long before the days of photographs), \
-        my first fancies regarding what they were like \
-        were unreasonably derived from their tombstones.
-        """,
-                                """
-        The shape of the letters on my father's, gave me \
-        an odd idea that he was a square, stout, dark man, \
-        with curly black hair.
-        """
-        ])
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        if let navigationController = window?.rootViewController as? UINavigationController,
-            let bookTableViewController = navigationController.topViewController as? BookTableViewController {
-            bookTableViewController.book = book
-        }
-        return true
-    }
 }
