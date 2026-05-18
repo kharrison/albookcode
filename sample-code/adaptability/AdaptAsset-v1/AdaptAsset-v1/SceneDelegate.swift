de9bf5c908f910c8926ad52d86c944dfd5e29796
Keith Harrison <keith@useyourloaf.com>
2026-05-18 17:03:23 +0100

    Update for Xcode 26, minimal target is iOS 15 Add scene delegate Enable approachable concurrency Add previews

--- sample-code/adaptability/AdaptAsset-v1/AdaptAsset-v1/SceneDelegate.swift
+++ sample-code/adaptability/AdaptAsset-v1/AdaptAsset-v1/SceneDelegate.swift
@@ -1,4 +1,4 @@
-//  Copyright © 2018 Keith Harrison. All rights reserved.
+//  Copyright © 2026 Keith Harrison. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
@@ -28,16 +28,6 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
 }
-
-#if DEBUG
-@available(iOS 17, *)
-#Preview {
-    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    let vc =
-    storyboard.instantiateViewController(withIdentifier:
-    "ViewController") as! ViewController
-    return vc
-}
-#endif
